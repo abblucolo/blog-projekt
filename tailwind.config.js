4cf5a209cefa519a6afbf3c2d9eed62b9a6d017a
abblucolo <92145431+abblucolo@users.noreply.github.com>
2022-04-23 20:14:15 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'sky': '#206494',
+        'footercolour1': '#124c75',
+        'footercolour2': '#0a3756',
+        'light-grey': '#acacac',
+        'grey': '#7b7b7b',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
